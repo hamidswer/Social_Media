460adab2c34a9a8212098dbbd62ad9c6632691ca
Hamid Zamaninasab <86551137+hamidswer@users.noreply.github.com>
2022-12-12 13:07:15 -0800

    Add files via upload

---
+++ client/src/Data/FollowersData.js
@@ -0,0 +1,10 @@
+import img1 from "../img/img1.png";
+import img2 from "../img/img2.png";
+import img3 from "../img/img3.png";
+import img4 from "../img/img4.jpg";
+export const Followers = [
+  { name: "Antoni Bandras", username: "AntoniBandras", img: img1 },
+  { name: "Bahman Moradi", username: "BahmanMoradi", img: img2 },
+  { name: "Negar Razavian", username: "NegarRazavian", img: img3 },
+  { name: "Jim Lee", username: "JimLee", img: img4 },
+];
